9c87e065ea969b6c78431d2146a978093bee05e5
Dilip Sundarraj <dilip.sundarraj@target.com>
2020-04-01 18:57:30 -0500

    removed the unwanted code

--- src/main/kotlin/com/learnfileupload/controller/FileUploadController.kt
+++ src/main/kotlin/com/learnfileupload/controller/FileUploadController.kt
@@ -2,24 +2,24 @@ package com.learnfileupload.controller
 
 import mu.KLogging
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder
-import java.io.IOException
 import javax.validation.constraints.NotNull
 
 
 @RestController
-@RequestMapping("/api/files")
+
 @CrossOrigin(value = ["*"], exposedHeaders = ["Content-Disposition"])
 class FileUploadController {
 
     companion object : KLogging()
 
-    @PostMapping
-    fun uploadNewFile(@NotNull @RequestParam("file") multipartFile: MultipartFile): ResponseEntity<String?>? {
+        @PostMapping("/fileUpload")
+    fun uploadNewFile(@NotNull @RequestParam("file") multipartFile: MultipartFile , @RequestParam("randomString") randomString: String): ResponseEntity<String?>? {
         logger.info("file original name : ${multipartFile.originalFilename}, multipart content type ${multipartFile.contentType}, File Size ${multipartFile.size}")
+        logger.info("randomString : $randomString")
         return ResponseEntity.status(HttpStatus.CREATED).build()
     }
 }
